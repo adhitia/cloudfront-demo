285ca07ab439010a5dd251ec8fd6789d634d3652
Adhitia Hidayat <adhitia.hidayat@gmail.com>
2016-11-15 14:43:41 +0700

    whitelist IP

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :block_foreign_hosts
+
+  def whitelisted?(ip)
+    return true if [103.43.128.42].include?(ip)
+    false
+  end
+
+  def block_foreign_hosts
+    return false if whitelisted?(request.remote_ip)
+    redirect_to "https://www.google.com"
+  end
+
+
 end
